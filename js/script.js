8295f689e2997ccb9cd7f42d22d4b83992cceebb
Evgeniya Gordeeva <g_enia@mail.ru>
2019-10-19 00:41:57 +0500

    Урок 2. Обязательное задание

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
-var money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
+let money = 30003,
+    income = 'фриланс',
+    addExpenses = 'абонемент в спортзал, кинотеатр, посиделки в кафе',
+    deposit = true,
+    mission = 100000,
+    period = 6;
 
-alert('Только сегодня!!! \rЗагляни в консоль и ты узнаешь какие переменные есть в проекте!');
+//Вывести в консоль тип данных значений переменных money, income, deposit;
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-console.log('В проекте существуют переменные: money, income, addExpenses, deposit, mission, period');
+//Вывести в консоль длину строки income
+console.log(income.length);
+
+//Вывести в консоль “Период (period) месяцев” и “Цель заработать (mission) рублей/долларов/гривен/юани”
+console.log('Период ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+//Привести строку addExpenses к нижнему регистру и разбить строку на массив, вывести массив в консоль
+console.log(addExpenses.toLowerCase().split(','));
+
+/*
+Объявить переменную budgetDay и присвоить дневной бюджет (доход за месяц / 30),
+вывести в консоль результат и остаток от деления
+*/
+let budgetDay = money / 30;
+console.log(budgetDay);
+console.log(money%30);
